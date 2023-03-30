6888a0f6ebd777b29dc353fa0e083a8334cb5552
Nayanna Nara <nayanna501@gmail.com>
2023-03-29 21:54:14 -0300

    add contrib base

---
+++ core/contrib/exceptions.py
@@ -0,0 +1,14 @@
+class BaseException(Exception):
+    message: str = 'Internal Server Error'
+
+    def __init__(self: 'BaseException', message: str | None = None) -> None:
+        if message:
+            self.message = message
+
+
+class ObjectNotFound(BaseException):
+    pass
+
+
+class DatabaseException(BaseException):
+    pass
